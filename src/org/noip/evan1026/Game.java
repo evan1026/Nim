30fd1f8c08d282f4f1a67808436c9a603d9f41ce
evan1026 <kkrockme4@gmail.com>
2013-05-31 10:48:37 -0400

    Doing stuff, will describe later

--- src/org/noip/evan1026/Game.java
+++ src/org/noip/evan1026/Game.java
@@ -4,13 +4,32 @@ import java.util.ArrayList;
 
 public class Game {
 
-	private Board _board;
-	private boolean player1 = true;
+	private Board           _board;
+	private ArrayList<Line> _lines   = new ArrayList<Line>();
+	private boolean         _player1 = true;
 
+	/**
+	 * Initializes the {@link Board} using it's default constructor.
+	 */
 	public Game(){
 		_board = new Board();
 	}
 
+	/**
+	 * Initializes it with a {@link Board} with a size of the parameter passed.
+	 * @param boardSize The size of the {@link Board}
+	 */
+	public Game(int boardSize){
+		_board = new Board(boardSize);
+	}
+	
+	/**
+	 * Attempts to remove the pieces and checks for a win.
+	 * @param rowIndex The row of the pieces to be removed.
+	 * @param startIndex The start index of the pieces to be removed.
+	 * @param length The amount of pieces to be removed.
+	 * @return True if pieces were removed, false otherwise.
+	 */
 	public boolean tryRemoveSelection(int rowIndex, int startIndex, int length){
 		if (canRemoveSelection(rowIndex, startIndex, length)){
 			removeSelection(rowIndex, startIndex, length);
@@ -22,9 +41,6 @@ public class Game {
 		return false;
 	}
 
-	public Game(int boardSize){
-		_board = new Board(boardSize);
-	}
 
 	/**
 	 * 
@@ -53,6 +69,14 @@ public class Game {
 		return _board.getPiece(row, index);
 	}
 
+	public void printBoard(){
+		System.out.println(_board.toString());
+	}
+	
+	public ArrayList<Line> getLines(){
+		return _lines;
+	}
+
 	//Make sure length is 1 based
 	private boolean canRemoveSelection(int rowIndex, int startIndex, int length){
 
@@ -65,42 +89,40 @@ public class Game {
 
 		return canRemove;
 	}
-	
+
 	private void removeSelection(int rowIndex, int startIndex, int length){
 		for (int i = startIndex; i < startIndex + length; i++){
 			_board.setPiece(rowIndex, i, false);
 		}
-		player1 = !player1;
+		_lines.add(new Line(rowIndex, startIndex, startIndex + length));
+		_player1 = !_player1;
 	}
-	
-	public void printBoard(){
-		System.out.println(_board.toString());
-	}
-	
+
 	private boolean checkForWin(){
-	    
-	    boolean win = true;
-	    
-	    for (boolean[] row : getBoard()){
-	        for (int i = 0; i < row.length; i++){
-	            if (row[i]){
-	                win = false;
-	                break;
-	            }
-	        }
-	    }
-	    
-	    return win;
+
+		boolean win = true;
+
+		for (boolean[] row : getBoard()){
+			for (int i = 0; i < row.length; i++){
+				if (row[i]){
+					win = false;
+					break;
+				}
+			}
+		}
+
+		return win;
 	}
-	
+
 	private void doWin(){
-	    if (player1){
-	        //player1 lost
-	        System.out.println("Player 2 wins!");
-	    }
-	    else{
-	        //player2 lost
-	        System.out.println("Player 1 wins!");
-	    }
+		if (_player1){
+			//player1 lost
+			System.out.println("Player 2 wins!");
+		}
+		else{
+			//player2 lost
+			System.out.println("Player 1 wins!");
+		}
 	}
+
 }
